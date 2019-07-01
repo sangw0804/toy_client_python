2bbaf6eb7236b1bec7d6af0a92544fa26a63e5e2
SangWoo <sangw0804@likelion.org>
2019-07-01 14:32:14 +0900

    에러처리

--- client.py
+++ client.py
@@ -8,18 +8,28 @@ client = ftplib.FTP(host, user, password)
 
 print(f'successfully log in at {host} as {user}')
 while True:
-  cmd = input('')
+  try:
+    cmd = input('').split()
 
-  if cmd == 'exit':
-    client.quit()
-    break
-  elif cmd == 'ls':
-    client.dir()
-  elif cmd == 'pwd':
-    print(client.pwd())
-  elif cmd[:2] == 'cd':
-    print(client.cwd(cmd[3:]))
-  elif cmd[:5] == 'mkdir':
-    print(client.mkd(cmd[6:]))
-  else:
-    print('Invalid command!!!')
+    if cmd[0] == 'exit':
+      client.quit()
+      break
+    elif cmd[0] == 'ls':
+      client.dir()
+    elif cmd[0] == 'pwd':
+      print(client.pwd())
+    elif cmd[0] == 'cd':
+      print(client.cwd(cmd[1]))
+    elif cmd[0] == 'mkdir':
+      print(client.mkd(cmd[1]))
+    elif cmd[0] == 'rm':
+      print(client.rmd(cmd[1]))
+    elif cmd[0] == 'send':
+      print(client.storbinary(f'STOR {cmd[1]}', open(cmd[1], 'rb')))
+    elif cmd[0] == 'get':
+      with open(cmd[1], 'wb') as f:
+        print(client.retrbinary(f'RETR {cmd[1]}', f.write))
+    else:
+      print('Invalid command!!!')
+  except Exception as E:
+    print(E)
