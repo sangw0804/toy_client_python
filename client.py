396db9f1d5ac964186b1c4c0ad881ccdecdbab33
SangWoo <sangw0804@likelion.org>
2019-07-01 13:50:33 +0900

    기능 추가

--- client.py
+++ client.py
@@ -4,8 +4,22 @@ host = input('host: ')
 user = input('user: ')
 password = input('password: ')
 
-print(user, password)
-client = ftplib.FTP(host)
-client.login(user, password)
-print(client.pwd())
-client.dir()
+client = ftplib.FTP(host, user, password)
+
+print(f'successfully log in at {host} as {user}')
+while True:
+  cmd = input('')
+
+  if cmd == 'exit':
+    client.quit()
+    break
+  elif cmd == 'ls':
+    client.dir()
+  elif cmd == 'pwd':
+    print(client.pwd())
+  elif cmd[:2] == 'cd':
+    print(client.cwd(cmd[3:]))
+  elif cmd[:5] == 'mkdir':
+    print(client.mkd(cmd[6:]))
+  else:
+    print('Invalid command!!!')
